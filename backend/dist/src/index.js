821dc16ac5968b343660e1f87986e0a33b1049bb
Shaik <shaikarshad1792004@gmail.com>
2024-08-03 00:12:34 +0530

    fix: cors error

--- backend/dist/src/index.js
+++ backend/dist/src/index.js
@@ -19,7 +19,7 @@ const extension_accelerate_1 = require("@prisma/extension-accelerate");
 const zod_1 = require("zod");
 const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+app.use("/*", (0, cors_1.default)());
 const port = 3000;
 app.use(body_parser_1.default.json());
 // only used to populate data in the database
