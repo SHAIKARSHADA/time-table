c842cac296fcdd49b64681e6c91f4488903854d9
Shaik <shaikarshad1792004@gmail.com>
2024-08-04 14:39:43 +0530

    deploy: deploying to vercel

--- frontend/src/components/Text.tsx
+++ frontend/src/components/Text.tsx
@@ -6,7 +6,9 @@ export const Text = () => {
 
   if(loading) {
     return(
-        <p className=" text-center text-xl md:text-5xl md:leading-tight pb-20 md:pb-5 pt-0 font-bold  z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">Loading....</p>
+        <p className="  animate-pulse flex flex-col items-center justify-center text-center text-xl md:text-5xl md:leading-tight pb-20 md:pb-5 pt-0 font-bold  z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+          <div className="h-10 ms-2   bg-gray-600 rounded-full  w-80"></div>
+        </p>
     )
   }
 
