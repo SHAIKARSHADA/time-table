b8c9166d98e55cea85799ae9a717c8c792bb0daf
Shaik <shaikarshad1792004@gmail.com>
2024-08-12 09:40:15 +0530

    update: added vercel analytics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { Text } from "./components/Text";
 import { Timetable } from "./components/TimeTables"
+import { Analytics } from "@vercel/analytics/react"
 
 
 function App() {
   return (
     <>
+    <Analytics/>
       <div className="h-screen max-w-full  bg-black bg-grid-white/[0.2] flex flex-col  justify-center overflow-hidden"> 
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center  bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_80%,black)]"></div> 
       <Text/>
